58b753bb750261d9a06f38a83cbbebfbc4221a4d
Stan0men <stan0men@yahoo.com>
2023-07-05 09:33:57 +0300

    add main files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import Donation from "./components/Donation";
+import GameList from "./components/GameList";
+import SearchBar from "./components/SearchBar";
+import SearchResult from "./components/SearchResult";
+import TopMenu from "./components/TopMenu";
+
+const sheetID = "1R5w9sRSGOEuilUo6n8K3r1bDCGebv50jFejt1bOMZY0";
+const URL = "https://docs.google.com/spreadsheets/d/" + sheetID;
+// fetch(URL)
+//   .then((res) => res.text())
+//   .then((rep) => {
+//     let data = JSON.parse(rep.substr(47).slice(0, -2));
+//     console.log(rep);
+//   });
+
+console.log(URL);
+
 const App = () => {
-  return <div>"Hello Chort"</div>;
+  return (
+    <div>
+      <TopMenu />
+      <SearchBar />
+      <GameList />
+      <SearchResult />
+      <Donation />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/GameList.js
@@ -0,0 +1,14 @@
+import Game from "./Game";
+
+const GameList = () => {
+  return (
+    <div>
+      GameList
+      <Game />
+      <Game />
+      <Game />
+    </div>
+  );
+};
+
+export default GameList;
